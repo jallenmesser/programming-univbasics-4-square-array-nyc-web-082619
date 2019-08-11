ac51bba16872532484c98e023cfdd3b0af5976c2
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-11 17:24:45 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   i = 0 
   sq_array = []
   while i < array.length do
-    sq_array.push(array[i] **)
+    sq_array.push(array[i] ** 2)
     i += 1
   end
   return sq_array
